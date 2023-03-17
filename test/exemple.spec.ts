e6e81c5e926f8a7949a3c037df3e736bcce11792
Estudos <estudos@Abners-MacBook-Pro.local>
2023-03-17 03:53:03 -0300

    Iniciando test

---
+++ test/exemple.spec.ts
@@ -0,0 +1,14 @@
+import { test, expect } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
+
+test('It should create new transaction', async () => {
+  await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New Transactions',
+      amount: 5000,
+      type: 'credit',
+    })
+    .expect(201)
+})
