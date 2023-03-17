905f145db44151acc4ff255e5163e15d33b4c317
Estudos <estudos@Abners-MacBook-Pro.local>
2023-03-17 02:31:13 -0300

    Config Cookie

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,13 @@
 import fastify from 'fastify'
-import crypto from 'node:crypto'
-import { knex } from './database/database'
+import cookie from '@fastify/cookie'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  // const transaction = await knex('transactions')
-  //   .insert({
-  //     id: crypto.randomUUID(),
-  //     title: 'Transação de teste',
-  //     amount: 500,
-  //   })
-  //   .returning('*')
-  // return transaction
-  // Busca
-  // const transactions = await knex('transactions').select('*')
-  // return transactions
-  // const transactions = await knex('transactions')
-  //   .where('amount', 500)
-  //   .select('*')
-  // return transactions
+app.register(cookie)
+
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app
